5768cba17b484a46146c15be07c81bdfe92af25a
Ombuweb <tjiheron@gmail.com>
2025-07-18 15:42:45 +0200

    fix(lint): Prefer using a top-level type-only

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,5 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import {  a, defineData } from '@aws-amplify/backend';
+import type {ClientSchema} from '@aws-amplify/backend';
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
