6b1640a17961b8eec41ab4057b59070aa2036e4d
Ombuweb <tjiheron@gmail.com>
2025-07-19 23:23:46 +0200

    Add Auth with AWS Amplify(Cognito)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+  import { Amplify } from 'aws-amplify';
+  import outputs from '../amplify_outputs.json';
+
+  Amplify.configure(outputs);
+</script>
